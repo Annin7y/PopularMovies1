297989977cf8a6b187f4501e1f1d1074a44b9e5d
AnaVox6y <ana_35an@yahoo.ca>
2017-06-04 13:31:24 -0400

    update code

--- app/src/main/java/com/example/android/popularmovies1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies1/MainActivity.java
@@ -1,18 +1,50 @@
 package com.example.android.popularmovies1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * Base URL for the image
      */
     private static final String BASE_URL = "http://image.tmdb.org/t/p/w185";
 
+    private ImageView imageView;
+
+    private RecyclerView mRecyclerView;
+
+    private MovieAdapter mMoviesAdapter;
+
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load(BASE_URL)
+                .into(imageView);
+
     }
 }
--- app/src/main/java/com/example/android/popularmovies1/MovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovies1/MovieAdapter.java
@@ -1,8 +1,46 @@
 package com.example.android.popularmovies1;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Maino96-10022 on 6/3/2017.
  */
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private ArrayList<Movie> moviesList;
+
+    public MovieAdapter(ArrayList<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder {
+
+    }
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+    }
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+
+
+    @Override
+    public int getItemCount() {
+        if (null == moviesList) return 0;
+        return moviesList.size();
+    }
 }
+
+
+
